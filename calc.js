9c1b6442a6eee4ee7471b2513afb439adc576036
Ian Cooperman <i.perry.leonard.c@gmail.com>
2018-12-16 16:39:16 -0800

    Finished rudimentary statement count parsing.

--- calc.js
+++ calc.js
@@ -1,29 +1,33 @@
-function post_score() {
+function post_score(rating) {
     var honesty_chart = document.getElementsByClassName('peepDetailRuling')[0];
     console.log(honesty_chart);
     var score_display = document.createElement('p');
 
-    score_display.innerHTML = "Test Score Please Confirm";
+    score_display.innerHTML = "This politician gets a rating of " + rating;
 
-    honesty_chart.parentNode.insertBefore(score_display);
+    honesty_chart.insertAdjacentElement("afterend", score_display);
 }
 
 function get_ratings() {
+    var ratingRegEx = /^(\d+).*/;
+
     var ratingsElems = document.getElementsByClassName("chartlist__count");
-    console.log(ratingsElems);
 
     var ratings = [];
 
-    
     for (var i = 0; i < ratingsElems.length; i++) {
-        var split_elem = ratings[i].outerText.split(" ");
-        console.log(split_elem);
-
-        ratings.push(split_elem[0]);
+        var txt = ratingsElems[i].innerText;
+        var rating = txt.split(" ")[0];
+        
+        ratings.push(rating);
     }
 
     console.log(ratings);
+
+    return ratings;
+
 }
 
-alert("Extension loaded.")
+// alert("Extension loaded.");
 get_ratings();
+// post_score("0");
